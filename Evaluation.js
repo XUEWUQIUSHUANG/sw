85f01693353124cf3f9e8a8174884aaa37ba9c7f
Frost Autumn <78307204+XUEWUQIUSHUANG@users.noreply.github.com>
2024-06-23 23:38:41 +0800

    Update Evaluation.js

--- Evaluation.js
+++ Evaluation.js
@@ -1,11 +1,11 @@
 for (const i in $('.jqx-radiobutton-input')) {
-        if (i % 5 === 0)
-            $('.jqx-radiobutton-input')[i].click();
-    }
-    for (var i = 0; i < $('#wjtx-root')[0].__vue__._data.teachers.length; i++) {
-        $('#wjtx-root')[0].__vue__._data.teachers[i].DA[11].DA = 'nice';
-    }
-    $("button:nth-child(2)").click();
-    setTimeout(() => {
-        $('.bh-color-primary-5')[0].click();
-    }, 100);
+    if (i % 5 === 0)
+        $('.jqx-radiobutton-input')[i].click();
+}
+for (var i = 0; i < $('#wjtx-root')[0].__vue__._data.teachers.length; i++) {
+    $('#wjtx-root')[0].__vue__._data.teachers[i].DA[11].DA = 'nice';
+}
+$("button:nth-child(2)").click();
+setTimeout(() => {
+    $('.bh-color-primary-5')[0].click();
+}, 100);
